f92f26635a9a2b6d4c3521d84a8b784e404e2121
mikha-dev <mikha.dev@gmail.com>
2023-12-07 09:50:42 +0300

    enums

--- src/CodemirrorMode.php
+++ src/CodemirrorMode.php
@@ -2,12 +2,12 @@
 
 namespace Dcat\Admin;
 
-use Dcat\Admin\DcatEnum;
-use Dcat\Admin\Traits\DcatEnumTrait;
+use D4T\Core\Contracts\D4TEnum;
+use D4T\Core\Traits\D4TEnumTrait;
 
-enum CodemirrorMode: string implements DcatEnum
+enum CodemirrorMode: string implements D4TEnum
 {
-    use DcatEnumTrait;
+    use D4TEnumTrait;
 
     case PHP = 'text/x-php';
     case CSS = 'text/css';
